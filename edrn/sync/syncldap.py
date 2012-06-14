c42c0eb43ca01cbad2eae4e8c75db33f3d47c4ac
Sean Kelly <kelly@seankelly.biz>
2012-06-14 20:13:10 +0000

    comsetic: != is preferred, <> is obsolete

--- edrn/sync/syncldap.py
+++ edrn/sync/syncldap.py
@@ -3,18 +3,18 @@
 # Copyright 2011 California Institute of Technology. ALL RIGHTS
 # RESERVED. U.S. Government Sponsorship acknowledged.
 
-u'''EDRN generic LDAP functions.
+'''EDRN generic LDAP functions.
 '''
 
 import ldap
 
 def personExists(ldapConn, uid):
     baseDn = "dc=edrn,dc=jpl,dc=nasa,dc=gov"
-    filter="(uid="+uid+")"
-    attrs=[]
+    searchFilter = "(uid="+uid+")"
+    attrs = []
     try:
-        results = ldapConn.search_s(baseDn, ldap.SCOPE_SUBTREE, filter, attrs)
-        if results <> None and len(results) > 0:
+        results = ldapConn.search_s(baseDn, ldap.SCOPE_SUBTREE, searchFilter, attrs)
+        if results != None and len(results) > 0:
             return True
         else:
             return False
@@ -25,11 +25,11 @@ def personExists(ldapConn, uid):
 
 def groupExists(ldapConn, groupcn):
     baseDn = "dc=edrn,dc=jpl,dc=nasa,dc=gov"
-    filter = "(&(cn="+groupcn+")(objectClass=groupOfUniqueNames))"
-    attrs=[]
+    searchFilter = "(&(cn="+groupcn+")(objectClass=groupOfUniqueNames))"
+    attrs = []
     try:
-        results = ldapConn.search_s(baseDn, ldap.SCOPE_SUBTREE, filter, attrs)
-        if results <> None and len(results) > 0:
+        results = ldapConn.search_s(baseDn, ldap.SCOPE_SUBTREE, searchFilter, attrs)
+        if results != None and len(results) > 0:
             return True
         else:
             return False
@@ -39,11 +39,11 @@ def groupExists(ldapConn, groupcn):
 
 def memberExists(ldapConn, groupcn, uid):
     baseDn = "dc=edrn,dc=jpl,dc=nasa,dc=gov"
-    filter = "(&(cn="+groupcn+")(uniquemember="+uid+",dc=edrn,dc=jpl,dc=nasa,dc=gov))"
-    attrs=[]
+    searchFilter = "(&(cn="+groupcn+")(uniquemember="+uid+",dc=edrn,dc=jpl,dc=nasa,dc=gov))"
+    attrs = []
     try:
-        results = ldapConn.search_s(baseDn, ldap.SCOPE_SUBTREE, filter, attrs)
-        if results <> None and len(results) > 0:
+        results = ldapConn.search_s(baseDn, ldap.SCOPE_SUBTREE, searchFilter, attrs)
+        if results != None and len(results) > 0:
             return True
         else:
             return False
